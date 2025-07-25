3d11878660b5dddd5eef729560e5ec9d1e7aa401
aniketchauhan18 <workwithaniket18@gmail.com>
2025-07-25 11:59:22 +0000

    feat: add homepage

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { fontPoppins } from "@/lib/fonts";
+
+export const metadata: Metadata = {
+  title: "Paycasso.in",
+  description: "The future of money we build together",
+  icons: {
+    icon: ["/favicon.ico?v=2"],
+    apple: ["/apple-touch-icon.png?v=4"],
+    shortcut: ["/apple-touch-icon.png"],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${fontPoppins.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
